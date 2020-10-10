8473418eff5c94dd5a84ec4d15c06ef2a3cb247c
viral-lakhani <vlakhani@usc.edu>
2020-10-10 16:10:05 -0700

    Updates

---
+++ precache-manifest.4ef841990fc6483ecd6589348ac1676e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1093453b57ffaee1e0617b677f3cde1c",
+    "url": "/news-app/index.html"
+  },
+  {
+    "revision": "be200bc7649c4da93460",
+    "url": "/news-app/static/css/2.f39ea103.chunk.css"
+  },
+  {
+    "revision": "2ff8c373c64a0d0dd222",
+    "url": "/news-app/static/css/main.8abd67a6.chunk.css"
+  },
+  {
+    "revision": "be200bc7649c4da93460",
+    "url": "/news-app/static/js/2.5ecf886b.chunk.js"
+  },
+  {
+    "revision": "9180c1b8cfdccb4dd884b4359354a332",
+    "url": "/news-app/static/js/2.5ecf886b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ff8c373c64a0d0dd222",
+    "url": "/news-app/static/js/main.b66ac470.chunk.js"
+  },
+  {
+    "revision": "15f6d11b422c48aed45c",
+    "url": "/news-app/static/js/runtime-main.c366bf89.js"
+  }
+]);
