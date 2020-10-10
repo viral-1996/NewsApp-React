e1b1305f34801080be6adc6497f594845fe2492e
viral-lakhani <vlakhani@usc.edu>
2020-10-10 16:36:31 -0700

    Updates

---
+++ precache-manifest.7b50de9056bb72c99a0fa9a909b9db1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8d361e6e3135b471abe6d01a5272c4",
+    "url": "/NewsApp-React/index.html"
+  },
+  {
+    "revision": "1da18e8655731a814dbb",
+    "url": "/NewsApp-React/static/css/2.f39ea103.chunk.css"
+  },
+  {
+    "revision": "cde217d9672eda42a767",
+    "url": "/NewsApp-React/static/css/main.8abd67a6.chunk.css"
+  },
+  {
+    "revision": "1da18e8655731a814dbb",
+    "url": "/NewsApp-React/static/js/2.c45f9e32.chunk.js"
+  },
+  {
+    "revision": "9180c1b8cfdccb4dd884b4359354a332",
+    "url": "/NewsApp-React/static/js/2.c45f9e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde217d9672eda42a767",
+    "url": "/NewsApp-React/static/js/main.20eea3e5.chunk.js"
+  },
+  {
+    "revision": "f6a3e9e68c804b85aecd",
+    "url": "/NewsApp-React/static/js/runtime-main.7c543ee5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/news-app/precache-manifest.75d229a3e4dfe2d3dae652228ec5dd1c.js"
+  "/NewsApp-React/precache-manifest.7b50de9056bb72c99a0fa9a909b9db1e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/news-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NewsApp-React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
